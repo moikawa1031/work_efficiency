30b823596bfc21626f0b10b4258435a23a2a43d8
mo <eternal-f@hotmail.co.jp>
2023-07-09 10:22:57 +0900

    Update 4_2_open_excel_file_save.py

--- 4_2_open_excel_file_save.py
+++ 4_2_open_excel_file_save.py
@@ -4,6 +4,6 @@ import openpyxl
 wb = openpyxl.load_workbook("C:\\work\\CB_VMList.xlsx")
 
 ws = wb.active
-ws["A1"] = "Nakamura"
+ws["A1"] = "123"
 wb.save("C:\\work\\CB_VMList_save.xlsx")
 print(ws["A1"].value)
